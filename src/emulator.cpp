59fcdd0fa1b040d21e8d869eec161bc3e5e003b6
neverbrokeagainyb25-max <neverbrokeagainyb25@gmail.com>
2026-07-24 18:41:57 -0400

    Update emulator.cpp

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -1,1 +1,17 @@
+#include "emulator.hpp"
 
+#include <iostream>
+
+namespace nds {
+
+bool Emulator::initialize() {
+    std::cout << "Initializing Nintendo DS emulator...\n";
+    return true;
+}
+
+void Emulator::run() {
+    std::cout << "Emulator is running.\n";
+    std::cout << "No ROM loaded.\n";
+}
+
+} // namespace nds
